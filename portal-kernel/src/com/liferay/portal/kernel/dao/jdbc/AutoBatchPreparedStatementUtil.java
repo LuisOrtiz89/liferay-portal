121581c17709cd281f25f0ed0ca0337626c57e2d
Luis Ortiz <luis.ortiz@liferay.com>
2022-11-07 16:16:21 +0100

    LPS-163976 Improvement on logs

--- portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/AutoBatchPreparedStatementUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/AutoBatchPreparedStatementUtil.java
@@ -18,10 +18,8 @@ import com.liferay.petra.concurrent.NoticeableExecutorService;
 import com.liferay.petra.concurrent.NoticeableFuture;
 import com.liferay.petra.executor.PortalExecutorManager;
 import com.liferay.petra.function.UnsafeConsumer;
-import com.liferay.portal.kernel.dao.db.BaseDBProcess;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
@@ -122,9 +120,10 @@ public class AutoBatchPreparedStatementUtil {
 			if (++_count >= _HIBERNATE_JDBC_BATCH_SIZE) {
 				_count = 0;
 
-				_log.info("Start Batch Execution on thread because batch size " + Thread.currentThread().getId());
-				localPreparedStatement.executeBatch();
-				_log.info("Finish Batch Execution on thread because batch size " + Thread.currentThread().getId());
+				try (LoggingTimer loggingTimer = new LoggingTimer(
+					"Batch Execution on thread because batch size " + Thread.currentThread().getId())) {
+					localPreparedStatement.executeBatch();
+				}
 			}
 		}
 
@@ -136,11 +135,11 @@ public class AutoBatchPreparedStatementUtil {
 				PreparedStatement localPreparedStatement =
 					getPreparedStatement();
 
-				_log.info("Start Batch Execution on thread because manual execution " + Thread.currentThread().getId());
-				int [] result = localPreparedStatement.executeBatch();
-				_log.info("Finish Batch Execution on thread because manual execution " + Thread.currentThread().getId());
-
-				return result;
+				try (LoggingTimer loggingTimer = new LoggingTimer(
+					"Batch Execution on thread because batch size " + Thread.currentThread().getId())) {
+					int [] result = localPreparedStatement.executeBatch();
+					return result;
+				}
 			}
 
 			return new int[0];
@@ -241,10 +240,9 @@ public class AutoBatchPreparedStatementUtil {
 			NoticeableFuture<Void> noticeableFuture =
 				_noticeableExecutorService.submit(
 					() -> {
-						try {
-							_log.info("Starting Batch Execution on thread on async mode " + Thread.currentThread().getId());
+						try (LoggingTimer loggingTimer = new LoggingTimer(
+							"Batch Execution on thread on async mode " + Thread.currentThread().getId())) {
 							actionUnsafeConsumer.accept(localPreparedStatement);
-							_log.info("Finished Batch Execution on thread on async mode " + Thread.currentThread().getId());
 						}
 						finally {
 							localPreparedStatement.close();
@@ -368,7 +366,4 @@ public class AutoBatchPreparedStatementUtil {
 		private final String _sql;
 
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		AutoBatchPreparedStatementUtil.class);
 }
