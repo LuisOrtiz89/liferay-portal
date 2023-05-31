10c289f6785333e849a31e110585c2d6fe7e2b6e
Luis Ortiz <luis.ortiz@liferay.com>
2023-05-31 16:43:01 +0200

    test2

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/report/UpgradeReport.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/report/UpgradeReport.java
@@ -460,9 +460,9 @@ public class UpgradeReport {
 
 				return longestRunningUpgradeProcesses;
 			}
-		).put(
-			"errors", _getMessagesPrinters(upgradeRecorder.getErrorMessages())
 		)/*.put(
+			"errors", _getMessagesPrinters(upgradeRecorder.getErrorMessages())
+		).put(
 			"warnings",
 			_getMessagesPrinters(upgradeRecorder.getWarningMessages())
 		)*/.build();
