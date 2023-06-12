4733da02f78e553ea25a361f1c2a2bda62299435
Luis Ortiz <luis.ortiz@liferay.com>
2023-06-12 16:49:08 +0200

    LPS-186380 Adding new parameter to shell script to allow setting the JVM memory to be used by the tool

--- modules/util/portal-tools-db-partition-virtual-instance-migrator/scripts/db_partition_virtual_instance_migrator.sh
+++ modules/util/portal-tools-db-partition-virtual-instance-migrator/scripts/db_partition_virtual_instance_migrator.sh
@@ -22,4 +22,25 @@ cd "$(dirname "${DB_PARTITION_MIGRATOR_PATH}")"
 # Run database virtual instance migrator tool.
 #
 
-java -jar com.liferay.portal.tools.db.partition.virtual.instance.migrator.jar "$@"
+JVM_ARGS=""
+PROGRAM_ARGS=""
+
+ARGS=( "$@" )
+
+for i in "${!ARGS[@]}"; do
+case "${ARGS[i]}" in
+	'') # Skip if element is empty (happens when it's unsetted before)
+	  continue ;;
+	-mem|--memory) # Memory setting argument. Unsetting value as we deal with it at this moment
+	  param="${ARGS[i+1]}"
+	  JVM_ARGS="-Xms${param} -Xmx${param}"
+	  unset 'ARGS[i+1]'
+	  continue ;;
+	*) # If argument has not been matched it is a program argument
+	  PROGRAM_ARGS="${PROGRAM_ARGS} ${ARGS[i]}"
+	  continue ;;
+esac
+unset 'ARGS[i]'
+done
+
+java -jar $JVM_ARGS com.liferay.portal.tools.db.partition.virtual.instance.migrator.jar $PROGRAM_ARGS
