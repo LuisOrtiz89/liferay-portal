b9cfe91e786f40e2dfdce03cd877ab7005380b99
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-07-05 13:10:44 +0200

    LPS-185207 SF

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeClassName.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeClassName.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.upgrade.v7_4_x;
 
 import com.liferay.portal.db.partition.DBPartitionUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Sofía Mendoza Gutiérrez
+ */
 
 public class UpgradeClassName extends UpgradeProcess {
 
@@ -11,4 +29,5 @@ public class UpgradeClassName extends UpgradeProcess {
 	}
 
 	private static final String tableName = "ClassName_";
+
 }
