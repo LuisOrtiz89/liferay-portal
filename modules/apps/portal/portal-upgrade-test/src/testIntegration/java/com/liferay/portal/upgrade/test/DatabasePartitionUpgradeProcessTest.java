183a4510430b84d1ba5840daac8b8cf0cdecd437
Luis Ortiz <luis.ortiz@liferay.com>
2023-07-10 18:35:27 +0200

    LPS-189289 Applying the changes to the tests using the property

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/DatabasePartitionUpgradeProcessTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/DatabasePartitionUpgradeProcessTest.java
@@ -16,15 +16,12 @@ package com.liferay.portal.upgrade.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.portal.db.partition.DBPartitionUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.util.PropsTestUtil;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeException;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.Props;
-import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.sql.Connection;
@@ -53,24 +50,34 @@ public class DatabasePartitionUpgradeProcessTest {
 
 	@BeforeClass
 	public static void setUpClass() {
-		_originalProps = PropsUtil.getProps();
+		_originalDatabasePartitionEnabledTest =
+			ReflectionTestUtil.getFieldValue(
+				DBPartitionUtil.class, "_databasePartitionEnabledTest");
+		_originalDatabasePartitionThreadPoolEnabled =
+			ReflectionTestUtil.getFieldValue(
+				DBPartitionUtil.class,
+				"_DATABASE_PARTITION_THREAD_POOL_ENABLED");
 	}
 
 	@AfterClass
 	public static void tearDownClass() {
-		PropsUtil.setProps(_originalProps);
+		ReflectionTestUtil.setFieldValue(
+			DBPartitionUtil.class, "_databasePartitionEnabledTest",
+			_originalDatabasePartitionEnabledTest);
+		ReflectionTestUtil.setFieldValue(
+			DBPartitionUtil.class, "_DATABASE_PARTITION_THREAD_POOL_ENABLED",
+			_originalDatabasePartitionThreadPoolEnabled);
 	}
 
 	@Test
 	public void testUpgradeWithDatabasePartitionDisabled()
 		throws UpgradeException {
 
-		PropsTestUtil.setProps(
-			HashMapBuilder.<String, Object>put(
-				"database.partition.enabled", "false"
-			).put(
-				"database.partition.thread.pool.enabled", "true"
-			).build());
+		ReflectionTestUtil.setFieldValue(
+			DBPartitionUtil.class, "_databasePartitionEnabledTest", false);
+		ReflectionTestUtil.setFieldValue(
+			DBPartitionUtil.class, "_DATABASE_PARTITION_THREAD_POOL_ENABLED",
+			true);
 
 		UpgradeProcess upgradeProcess = new AssertConnectionUpgradeProcess();
 
@@ -81,19 +88,19 @@ public class DatabasePartitionUpgradeProcessTest {
 	public void testUpgradeWithDatabasePartitionEnabled()
 		throws UpgradeException {
 
-		PropsTestUtil.setProps(
-			HashMapBuilder.<String, Object>put(
-				"database.partition.enabled", "true"
-			).put(
-				"database.partition.thread.pool.enabled", "true"
-			).build());
+		ReflectionTestUtil.setFieldValue(
+			DBPartitionUtil.class, "_databasePartitionEnabledTest", true);
+		ReflectionTestUtil.setFieldValue(
+			DBPartitionUtil.class, "_DATABASE_PARTITION_THREAD_POOL_ENABLED",
+			true);
 
 		UpgradeProcess upgradeProcess = new AssertConnectionUpgradeProcess();
 
 		upgradeProcess.upgrade();
 	}
 
-	private static Props _originalProps;
+	private static boolean _originalDatabasePartitionEnabledTest;
+	private static boolean _originalDatabasePartitionThreadPoolEnabled;
 
 	private class AssertConnectionUpgradeProcess extends DummyUpgradeProcess {
 
@@ -101,9 +108,7 @@ public class DatabasePartitionUpgradeProcessTest {
 		protected void process(UnsafeConsumer<Long, Exception> unsafeConsumer)
 			throws Exception {
 
-			if (GetterUtil.getBoolean(
-					PropsUtil.get("database.partition.enabled"))) {
-
+			if (DBPartitionUtil.isPartitionEnabled()) {
 				Assert.assertNotSame(_getConnection(), _getConnection());
 			}
 			else {
