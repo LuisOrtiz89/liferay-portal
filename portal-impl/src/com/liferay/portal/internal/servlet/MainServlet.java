03e1b207e5f356cb487c4ae2c39b290ece189224
Luis Ortiz <luis.ortiz@liferay.com>
2023-12-19 15:08:34 +0100

    LPS-194500 Now it is needed to initialize the PortalInstancePool at once

--- portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
 import com.liferay.portal.kernel.deploy.hot.HotDeployUtil;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
@@ -107,6 +108,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletContext;
@@ -782,6 +784,8 @@ public class MainServlet extends HttpServlet {
 			throw new RuntimeException("Company default web ID is null");
 		}
 
+		List<Company> companies = new CopyOnWriteArrayList<>();
+
 		CompanyLocalServiceUtil.forEachCompany(
 			company -> {
 				if (StartupHelperUtil.isDBNew() &&
@@ -789,12 +793,16 @@ public class MainServlet extends HttpServlet {
 						PropsValues.COMPANY_DEFAULT_WEB_ID,
 						company.getWebId())) {
 
-					PortalInstances.initCompany(company, true);
+					PortalInstances.initCompany(company, true, true);
 				}
 				else {
-					PortalInstances.initCompany(company, false);
+					PortalInstances.initCompany(company, false, true);
 				}
+
+				companies.add(company);
 			});
+
+		PortalInstancePool.add(companies);
 	}
 
 	private void _initLayoutTemplates(PluginPackage pluginPackage) {
