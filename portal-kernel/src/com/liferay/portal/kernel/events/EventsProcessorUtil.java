8e5c813f6b5f5ee78382b20743df58b35739d870
Luis Ortiz <luis.ortiz@liferay.com>
2024-01-31 12:47:33 +0100

    LPD-6609 Moving EventsProcessorUtil and WebAppPool to portal-kernel
    # breaking
    
    ## What portal-impl/src/com/liferay/portal/events/EventsProcessorUtil.java
    
    Class has been moved to portal-kernel.
    
    ## Why
    
    As this class contains core features, it is better to move it to portal-kernel to achieve accesibility from other classes.
    
    ## Alternatives
    
    Use com.liferay.portal.kernel.events.EventsProcessorUtil instead.
    
    ----
    
    # breaking
    
    ## What portal-impl/src/com/liferay/portal/util/WebAppPool.java
    
    Class has been moved to portal-kernel.
    
    ## Why
    
    As this class contains core features, it is better to move it to portal-kernel to achieve accesibility from other classes.
    
    ## Alternatives
    
    Use com.liferay.portal.kernel.util.WebAppPool instead.
    
    
    ----

--- portal-kernel/src/com/liferay/portal/kernel/events/EventsProcessorUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/events/EventsProcessorUtil.java
@@ -3,13 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.events;
+package com.liferay.portal.kernel.events;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.portal.kernel.events.ActionException;
-import com.liferay.portal.kernel.events.LifecycleAction;
-import com.liferay.portal.kernel.events.LifecycleEvent;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
