20fd769149f21e7707342abaaf12b11ef9a9afa4
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-06-20 19:19:17 +0200

    LPD-27658 This must be transactional now

--- portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
@@ -307,7 +307,6 @@ public class ResourceActionLocalServiceImpl
 	}
 
 	@Override
-	@Transactional(enabled = false)
 	public ResourceAction fetchResourceAction(String name, String actionId) {
 		ResourceAction resourceAction = _resourceActions.get(
 			encodeKey(name, actionId));
@@ -325,7 +324,6 @@ public class ResourceActionLocalServiceImpl
 	}
 
 	@Override
-	@Transactional(enabled = false)
 	public ResourceAction getResourceAction(String name, String actionId)
 		throws PortalException {
 
