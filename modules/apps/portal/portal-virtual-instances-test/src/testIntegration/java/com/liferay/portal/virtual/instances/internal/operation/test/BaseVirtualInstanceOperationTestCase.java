c0da7b41ab4b70c52d60213296840240896e6ff1
Luis Ortiz <luis.ortiz@liferay.com>
2025-02-04 16:14:44 +0100

    LPD-46406 Moving virtual instances tests to a new module

--- modules/apps/portal/portal-virtual-instances-test/src/testIntegration/java/com/liferay/portal/virtual/instances/internal/operation/test/BaseVirtualInstanceOperationTestCase.java
+++ modules/apps/portal/portal-virtual-instances-test/src/testIntegration/java/com/liferay/portal/virtual/instances/internal/operation/test/BaseVirtualInstanceOperationTestCase.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.db.partition.internal.operation.test;
+package com.liferay.portal.virtual.instances.internal.operation.test;
 
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
 import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
