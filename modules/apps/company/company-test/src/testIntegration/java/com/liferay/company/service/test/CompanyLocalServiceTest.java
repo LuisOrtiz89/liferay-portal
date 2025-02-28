3728a5e14c048341030b214c898c3d0419af16e5
Luis Ortiz <luis.ortiz@liferay.com>
2025-02-28 13:53:42 +0100

    LPD-46407 Now the extract operation is supported with and without database partitioning enabled

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -915,7 +915,7 @@ public class CompanyLocalServiceTest {
 	}
 
 	@Test
-	public void testExtractDBPartitionCompanyDefaultCompany() {
+	public void testExtractCompanyDefaultCompany() {
 		try {
 			_companyLocalService.extractCompany(
 				PortalInstancePool.getDefaultCompanyId());
@@ -923,14 +923,7 @@ public class CompanyLocalServiceTest {
 			Assert.fail();
 		}
 		catch (Exception exception) {
-			if (DBPartition.isPartitionEnabled()) {
-				Assert.assertTrue(
-					exception instanceof RequiredCompanyException);
-			}
-			else {
-				Assert.assertTrue(
-					exception instanceof UnsupportedOperationException);
-			}
+			Assert.assertTrue(exception instanceof RequiredCompanyException);
 		}
 	}
 
