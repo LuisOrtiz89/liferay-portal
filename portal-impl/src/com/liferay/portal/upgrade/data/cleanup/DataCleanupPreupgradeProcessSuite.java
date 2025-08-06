94ae1fc2018470d551e066f2dc3e0ecd0f843611
Luis Ortiz <luis.ortiz@liferay.com>
2025-08-06 13:55:03 +0200

    LPD-53135 Adding the new data cleanup preupgrade process to the DataCleanupPreupgradeProcessSuite

--- portal-impl/src/com/liferay/portal/upgrade/data/cleanup/DataCleanupPreupgradeProcessSuite.java
+++ portal-impl/src/com/liferay/portal/upgrade/data/cleanup/DataCleanupPreupgradeProcessSuite.java
@@ -93,6 +93,7 @@ public class DataCleanupPreupgradeProcessSuite {
 
 			//
 
+			new AnalyticsMessageDataCleanupPreupgradeProcess(),
 			new ConfigurationDataCleanupPreupgradeProcess(),
 			new DDMStructureDataCleanupPreupgradeProcess(),
 			new DLFileEntryDataCleanupPreupgradeProcess(),
