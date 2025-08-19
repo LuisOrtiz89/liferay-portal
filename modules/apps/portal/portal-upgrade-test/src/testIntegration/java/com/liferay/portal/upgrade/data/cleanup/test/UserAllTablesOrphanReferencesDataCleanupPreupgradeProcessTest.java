8b7f8a689262b8c001a63b13c044d3dc0b0dfb39
Luis Ortiz <luis.ortiz@liferay.com>
2025-08-19 17:50:49 +0200

    LPD-63152 As now the BaseAllTablesOrphanReferencesDataCleanupPreupgradeProcess class can print log messages, we need to capture those for the assertions in the tests

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/UserAllTablesOrphanReferencesDataCleanupPreupgradeProcessTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/UserAllTablesOrphanReferencesDataCleanupPreupgradeProcessTest.java
@@ -6,7 +6,7 @@
 package com.liferay.portal.upgrade.data.cleanup.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.petra.function.UnsafeBiConsumer;
 import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringBundler;
@@ -85,11 +85,11 @@ public class UserAllTablesOrphanReferencesDataCleanupPreupgradeProcessTest
 	}
 
 	@Override
-	protected UnsafeConsumer<LogCapture, Exception>
-		getLogAssertionUnsafeConsumer() {
+	protected UnsafeBiConsumer<LogCapture, LogCapture, Exception>
+		getLogAssertionUnsafeBiConsumer() {
 
-		return logCapture -> {
-			List<String> messages = logCapture.getMessages();
+		return (logCapture1, logCapture2) -> {
+			List<String> messages = logCapture1.getMessages();
 
 			Assert.assertTrue(
 				messages.contains(
@@ -110,6 +110,9 @@ public class UserAllTablesOrphanReferencesDataCleanupPreupgradeProcessTest
 						" was not found in the origin table ",
 						dbInspector.normalizeName("User_"), " and column ",
 						dbInspector.normalizeName("userId"))));
+
+			messages = logCapture2.getMessages();
+
 			Assert.assertTrue(
 				messages.contains(
 					StringBundler.concat(
