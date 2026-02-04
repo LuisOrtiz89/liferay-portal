1edc72a471a3e6fdfe4d11c168a88466d5c1bef6
Luis Ortiz <luis.ortiz@liferay.com>
2026-02-04 18:01:16 +0100

    LPD-69683 Cleaning up resource permissions from other data cleanup preupgrade process tests with the new ResourcePermissionDataCleanupPreupgradeProcess

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/DDMDataCleanupPreupgradeProcessTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/DDMDataCleanupPreupgradeProcessTest.java
@@ -26,14 +26,13 @@ import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.data.cleanup.util.OrphanReferencesDataCleanupUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -43,6 +42,7 @@ import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.upgrade.data.cleanup.DDMDataCleanupPreupgradeProcess;
+import com.liferay.portal.upgrade.data.cleanup.ResourcePermissionDataCleanupPreupgradeProcess;
 
 import java.sql.Connection;
 
@@ -88,9 +88,6 @@ public class DDMDataCleanupPreupgradeProcessTest
 	public void setUp() throws Exception {
 		_classNames = _classNameLocalService.getClassNames(
 			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-		_resourcePermissions =
-			_resourcePermissionLocalService.getResourcePermissions(
-				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 	}
 
 	@After
@@ -102,16 +99,6 @@ public class DDMDataCleanupPreupgradeProcessTest
 
 		for (ClassName className : classNames) {
 			_classNameLocalService.deleteClassName(className);
-		}
-
-		List<ResourcePermission> resourcePermissions = ListUtil.remove(
-			_resourcePermissionLocalService.getResourcePermissions(
-				QueryUtil.ALL_POS, QueryUtil.ALL_POS),
-			_resourcePermissions);
-
-		for (ResourcePermission resourcePermission : resourcePermissions) {
-			_resourcePermissionLocalService.deleteResourcePermission(
-				resourcePermission);
 		}
 	}
 
@@ -146,6 +133,11 @@ public class DDMDataCleanupPreupgradeProcessTest
 			group.getGroupId(), journalArticle.getArticleId());
 
 		_groupLocalService.deleteGroup(group);
+
+		UpgradeProcess upgradeProcess =
+			new ResourcePermissionDataCleanupPreupgradeProcess();
+
+		upgradeProcess.upgrade();
 	}
 
 	@Test
@@ -267,6 +259,11 @@ public class DDMDataCleanupPreupgradeProcessTest
 		upgrade();
 
 		_groupLocalService.deleteGroup(group);
+
+		UpgradeProcess upgradeProcess =
+			new ResourcePermissionDataCleanupPreupgradeProcess();
+
+		upgradeProcess.upgrade();
 	}
 
 	@Test
@@ -459,7 +456,6 @@ public class DDMDataCleanupPreupgradeProcessTest
 	private static List<ClassName> _classNames;
 	private static Connection _connection;
 	private static DBInspector _dbInspector;
-	private static List<ResourcePermission> _resourcePermissions;
 
 	@Inject
 	private ClassNameLocalService _classNameLocalService;
@@ -476,8 +472,5 @@ public class DDMDataCleanupPreupgradeProcessTest
 
 	@Inject
 	private Portal _portal;
-
-	@Inject
-	private ResourcePermissionLocalService _resourcePermissionLocalService;
 
 }
