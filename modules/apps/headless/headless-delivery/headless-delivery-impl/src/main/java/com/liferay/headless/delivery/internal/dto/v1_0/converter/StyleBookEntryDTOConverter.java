8465c1ddd89906c56e20e2e3b50dff804e6ba706
Luis Ortiz <luis.ortiz@liferay.com>
2026-04-10 12:54:34 +0200

    Simple DTOConverter for StyleBook, but it is not working because an API is needed

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/StyleBookEntryDTOConverter.java
@@ -0,0 +1,40 @@
+package com.liferay.headless.delivery.internal.dto.v1_0.converter;
+
+import com.liferay.headless.delivery.dto.v1_0.StyleBook;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.style.book.model.StyleBookEntry;
+import com.liferay.style.book.service.StyleBookEntryLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+	property = "dto.class.name=com.liferay.style.book.model.StyleBookEntry",
+	service = DTOConverter.class
+)
+public class StyleBookEntryDTOConverter
+	implements DTOConverter<StyleBookEntry, StyleBook> {
+
+	public String getContentType() {
+		return StyleBook.class.getSimpleName();
+	}
+
+	@Override
+	public StyleBook toDTO(DTOConverterContext dtoConverterContext)
+		throws Exception {
+
+		StyleBookEntry styleBookEntry =
+			_styleBookEntryLocalService.getStyleBookEntry(
+				(Long)dtoConverterContext.getId());
+
+		return new StyleBook() {
+			{
+				setKey(styleBookEntry::getStyleBookEntryKey);
+				setName(styleBookEntry::getName);
+			}
+		};
+	}
+
+	@Reference
+	private StyleBookEntryLocalService _styleBookEntryLocalService;
+}
